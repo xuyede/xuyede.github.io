b48497524540e1913f3e0a23f3d6855703107fc7
xuyede <will3virgo@163.com>
2019-05-14 21:19:54 +0800

    xyd

--- build/precache-manifest.ed181bfbe1ff733f705927d89a276189.js
+++ build/precache-manifest.ed181bfbe1ff733f705927d89a276189.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "593003de29b0f831c2e6a599696de156",
-    "url": "/index.html"
+    "url": "/build/index.html"
   },
   {
     "revision": "cd1b0ba23739a04b3279",
-    "url": "/static/css/main.5e4b4390.chunk.css"
+    "url": "/build/static/css/main.5e4b4390.chunk.css"
   },
   {
     "revision": "bd5f6d35bad159bb1c8c",
-    "url": "/static/js/2.386cdf06.chunk.js"
+    "url": "/build/static/js/2.386cdf06.chunk.js"
   },
   {
     "revision": "cd1b0ba23739a04b3279",
-    "url": "/static/js/main.569829d0.chunk.js"
+    "url": "/build/static/js/main.569829d0.chunk.js"
   },
   {
     "revision": "fdfcfda2d9b1bf31db52",
-    "url": "/static/js/runtime~main.c5541365.js"
+    "url": "/build/static/js/runtime~main.c5541365.js"
   }
 ]);
